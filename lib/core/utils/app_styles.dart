a3793188f90313c37db4589638cc6f999e16cdd3
Mohamed Tamer <mohamed.tamerrr79@gmail.com>
2025-09-23 00:17:53 +0300

    create styles file and the text widget

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static TextStyle styleRegular16 = TextStyle(
+    fontSize: 16,
+    color: Color(0xffFFFFFF),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle styleRegular14 = TextStyle(
+    fontSize: 14,
+    color: Color(0xffFFFFFF),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle styleRegular30 = TextStyle(
+    fontSize: 30,
+    color: Color(0xffFFFFFF),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle styleRegular18 = TextStyle(
+    fontSize: 18,
+    color: Color(0xffFFFFFF),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle styleRegular20 = TextStyle(
+    fontSize: 20,
+    color: Color(0xffFFFFFF),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle styleRegular26 = TextStyle(
+    fontSize: 26,
+    color: Color(0xffFFFFFF),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+  );
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/app_styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_item.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_item_list.dart';
@@ -11,7 +12,23 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [CustomAppBar(), CustomItemList()],
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomAppBar(),
+        CustomItemList(),
+
+        Padding(
+          padding: const EdgeInsets.only(
+            bottom: 25,
+            left: 30,
+            top: 50,
+          ),
+          child: Text(
+            'Best Seller',
+            style: AppStyles.styleRegular18,
+          ),
+        ),
+      ],
     );
   }
 }
