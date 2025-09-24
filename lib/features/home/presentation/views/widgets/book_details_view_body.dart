8e427574bff6296458ad8f1878b10543d8b36eca
Mohamed Tamer <mohamed.tamerrr79@gmail.com>
2025-09-24 22:31:01 +0300

    add custom book item to book body details

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_appbar_book_details.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_item.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -8,9 +9,21 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.sizeOf(context).width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(children: [CustomAppBarBookDetails()]),
+      child: Column(
+        children: [
+          CustomAppBarBookDetails(),
+          SizedBox(height: 22),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: width * .16,
+            ),
+            child: const CustomItem(),
+          ),
+        ],
+      ),
     );
   }
 }
