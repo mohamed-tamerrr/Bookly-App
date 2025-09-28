1d6ec5da4be97c4ebf9b43ffdb26124dc6d40666
Mohamed Tamer <mohamed.tamerrr79@gmail.com>
2025-09-28 23:33:22 +0300

    create home repo methods and add dartz

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,11 @@
+import 'package:bookly_app/core/utils/errors.dart';
+import 'package:dartz/dartz.dart';
+
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+
 abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>>
   fetchBestSellerBooks();
-  fetchBooks();
+
+  Future<Either<Failure, List<BookModel>>> fetchBooks();
 }
